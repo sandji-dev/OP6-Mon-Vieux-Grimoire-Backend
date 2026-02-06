fa5123cd3427906f38b02aefda12f54eaca84d55
ricky-keys <sandjon.ricky@gmail.com>
2026-02-07 00:19:41 +0100

    auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,25 @@
+// On importe jsonwebtoken
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        // On récupère le token après le mot "Bearer"
+        //  On récupère le token dans le header Authorization
         const token = req.headers.authorization.split(' ')[1];
-        // On le décode
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        // On récupère le userId dedans
+
+        //  On vérifie et décode le token avec la clé secrète
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        //  On récupère l'id de l'utilisateur depuis le token
         const userId = decodedToken.userId;
-        // ON LE RAJOUTE À LA REQUÊTE ! 
-        req.auth = { userId: userId };
-        
-        next(); // On passe à la suite (le contrôleur)
+
+        //  On ajoute l'userId à la requête
+        req.auth = { userId };
+
+        // Tout est OK → on passe à la suite(controllers)
+        next();
+
     } catch (error) {
+        // Si le token est absent ou invalide → accès refusé
         res.status(401).json({ error: 'Requête non authentifiée !' });
     }
 };
